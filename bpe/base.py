97be374ffe9dac7b1c6afc7ff0231ac823b95d69
Robin Hu <123852082+xhu703981@users.noreply.github.com>
2026-06-14 20:46:08 -0400

    Refactor Tokenizer and BPETokenizer classes
    Refactor Tokenizer and BPETokenizer classes for improved functionality and clarity. Update methods for merging tokens, training, and saving/loading vocab.

--- bpe/base.py
+++ bpe/base.py
@@ -1,188 +1,227 @@
+import regex as re 
+import json
+#import heapq
 import os
-import regex as re
-from collections import Counter
+import glob
+from multiprocessing import Pool 
 
-##To be able to train on a large datasets: I implemented
-##1. frequency counter: instead of dealing with the full length text, we only care about the words appeard, which, in this case, were stored as tuples
-##2. doubly linked list: to be able to perform merge, we treat each word as a dll, which also prevents cross-words tokenization 
+GPT2_REGEX = re.compile(r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+""")
 
-GPT2_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+class Tokenizer:
 
-class Node:
-    def __init__(self,val,freq):
-        self.next=None
-        self.prev=None
-        self.val=val
-        self.merged=False
-        self.freq = freq 
-
-def ids_to_dll(ids,freq=1):
-    head=Node(ids[0],freq)
-    current_node=head
-    for i in range(1, len(ids)):
-        new_node=Node(ids[i],freq)
-        current_node.next=new_node
-        new_node.prev=current_node
-        current_node=new_node
-    return head
-
-def build_pair_data(head):
-    pair_counts={}
-    pair_position={}
-    node=head
-    while node.next:
-        pair=(node.val,node.next.val)
-        pair_counts[pair]=pair_counts.get(pair,0)+1
-        if pair not in pair_position:
-            pair_position[pair]=set()
-        pair_position[pair].add(node)
-        node=node.next
-    return pair_counts, pair_position
-
-def merge(ids,pair,idx):
-    i=0
-    new_ids=[]
-    while i< len(ids):
-        if  i<len(ids)-1 and ids[i]==pair[0] and ids[i+1]==pair[1]:
-            i+=2
-            new_ids.append(idx)
-        else:
-            new_ids.append(ids[i])
-            i+=1
-    return new_ids
-
-def merge_node(pair,idx,pair_counts,pair_positions):
-    for node in list(pair_positions[pair]):
-        if node.merged or node.next is None or node.next.merged: continue
-        if (node.val,node.next.val)!=pair: continue
-        L=node.prev
-        R=node.next.next
-        if L:
-            if (L.val,node.val) in pair_counts:
-                pair_counts[(L.val,node.val)]-=node.freq
-                pair_positions[(L.val, node.val)].discard(L)
-                if pair_counts[(L.val, node.val)] <= 0:
-                    del pair_counts[(L.val, node.val)]
-                    del pair_positions[(L.val, node.val)]
-        if R:
-            if (node.next.val,R.val) in pair_counts:
-                pair_counts[(node.next.val,R.val)]-=node.freq
-                pair_positions[(node.next.val,R.val)].discard(node.next)
-                if pair_counts[(node.next.val, R.val)] <= 0:
-                    del pair_counts[(node.next.val, R.val)]
-                    del pair_positions[(node.next.val, R.val)]
-        if pair in pair_counts:
-            pair_counts[pair]-=node.freq
-            pair_positions[pair].discard(node)
-            if pair_counts[pair] <= 0:
-                del pair_counts[pair]
-                del pair_positions[pair]
-        old_next = node.next
-        node.val=idx
-        node.next=R
-        old_next.merged=True
-        if node.next:
-            node.next.prev=node
-        if L:
-            new_pair1=(L.val, idx)
-            if new_pair1 not in pair_positions: pair_positions[new_pair1]=set()
-            pair_counts[new_pair1] = pair_counts.get(new_pair1, 0) + node.freq
-            pair_positions[new_pair1].add(L)
-
-        if R:
-            new_pair2=(idx,R.val)
-            if new_pair2 not in pair_positions: pair_positions[new_pair2]=set()
-            pair_counts[new_pair2] = pair_counts.get(new_pair2, 0) + node.freq
-            pair_positions[new_pair2].add(node)
-
-class BPETokenizer:
     def __init__(self):
-        self.vocab={}
-        self.merge_rules={}
+        self.merge_rules = {}    # for encode
+        self.vocab = self._build_vocab() # for decode : bytes - string 
     
-    def train(self,data_dir,vocab_size):
-        word_freq=Counter()
-        for filename in os.listdir(data_dir):
-            if filename.endswith(".txt"):
-                with open(os.path.join(data_dir, filename), "r", encoding="utf-8") as f:
-                    for token in re.findall(GPT2_PATTERN,f.read()):
-                        word_freq[tuple(token.encode("utf-8"))]+=1
 
-        word_heads = {}
-        pair_counts = {}
-        pair_positions = {}
-        for word, freq in word_freq.items():
-            head = ids_to_dll(list(word),freq)
-            word_heads[word] = (head, freq)
-            node = head
-            while node.next:
-                pair = (node.val, node.next.val)
-                pair_counts[pair] = pair_counts.get(pair, 0) + freq
-                if pair not in pair_positions:
-                    pair_positions[pair] = set()
-                pair_positions[pair].add(node)
-                node = node.next
-
-        num_merges=vocab_size-256
-        for i in range(num_merges):
-            pair=max((p for p in pair_counts if pair_counts[p] > 0), key=lambda p: pair_counts[p])
-            merge_node(pair,256+i,pair_counts,pair_positions)
-            self.merge_rules[pair]=256+i
-            print(f"merge {i}: {pair} -> {256+i}")
+    def _build_vocab(self):
+        vocab ={}
         for i in range(256):
-            self.vocab[i]=bytes([i])
-        for pair, idx in self.merge_rules.items():
-            self.vocab[idx]=self.vocab[pair[0]]+self.vocab[pair[1]]
+            vocab[i] = bytes([i])
+        return vocab
     
+
+    def merge(self, ids, pair, idx):  # iterate through some ids, if pair in it, replace it with idx
+        new_ids = [] 
+        i = 0
+        while i < len(ids):
+            if i<len(ids) -1 and ids[i] == pair[0] and ids[i+1] == pair[1]:
+                new_ids.append(idx)
+                i+=2
+            else:
+                new_ids.append(ids[i])
+                i+=1
+        return tuple(new_ids) # tuple as key of dict 
+    
+
+    def train(self, data_dir, vocab_size):
+
+        word_freq = {}   # word frequency counter
+        line_cnt = 0
+        txt_files = sorted(glob.glob(os.path.join(data_dir, "*.txt")))
+        for file_idx, path in enumerate(txt_files):
+            with open(path, 'r', encoding='utf-8') as f:
+                for line in f:    #interator ; memory is cleared after done
+                    line_cnt += 1
+                    tokens = GPT2_REGEX.findall(line)
+                    tokens = [t for t in tokens if t] # null char
+                    for token in tokens:
+                        byte_tuple = tuple(token.encode('utf-8'))
+                        word_freq[byte_tuple] = word_freq.get(byte_tuple, 0) + 1  #'hello world' : {(104, 101, 108, 108, 111): 1, (32, 119, 111, 114, 108, 100): 1}
+            if file_idx % 100 ==0:
+                print(f'finished : {file_idx} files')
+
+
+        pair_freq = {}  #token pair frequencey counter
+        pair_to_words = {}  # looking for token pair in all words is very expensive; we create this dict as an index to know what words to look at for particular tokens 
+    
+
+        for byte_tuple, freq in word_freq.items():
+            for i in range (len(byte_tuple) -1):
+                pair = (byte_tuple[i], byte_tuple[i+1])
+                pair_freq[pair] = pair_freq.get(pair, 0) + freq   # ‘the cat the dog' : {(116, 104): 2, (104, 101): 2, (32, 99): 1, (99, 97): 1, (97, 116): 1, (32, 116): 1, (32, 100): 1, (100, 111): 1, (111, 103): 1}
+
+                if pair not in pair_to_words:
+                    pair_to_words[pair] = set()
+                pair_to_words[pair].add(byte_tuple)
+
+        #heap = [(-freq, pair) for pair, freq in pair_freq.items()] #built-in heap only supports min-heap 
+        #heapq.heapify(heap)
+
+        for i in range(vocab_size - 256): # number of merges
+            print(f'merging step {i}')
+            if not pair_freq: # no pairs to merge
+                break
+
+            # while heap:
+            #     freq, best_pair = heapq.heappop(heap)
+            #     actual_freq = pair_freq.get(best_pair, 0) 
+            #     if actual_freq == -freq:  #lazy deletion ： checking if the best pair is valid (pair might have been affected)
+            #         break
+            # else:
+            #     break    # the heap is empty
+        
+            new_id = i + 256
+            best_pair = max(pair_freq, key=pair_freq.get)
+
+            self.merge_rules[best_pair] = new_id 
+            self.vocab[new_id] = self.vocab[best_pair[0]] + self.vocab[best_pair[1]]
+            
+            # find affected words from affected tokens
+            affected_words = pair_to_words.pop(best_pair, set())
+            if not affected_words:
+                continue
+
+            #updating pair_freq, but only for affected words
+            #removing the contribution of old words
+            for w in affected_words:
+                f = word_freq[w]  # frequencey of the word
+                for p in range(len(w)-1):
+                    pair = (w[p], w[p+1])
+                    pair_freq[pair] -= f #upadtaing token pair frequency 
+                    if pair_freq[pair]==0:
+                        del pair_freq[pair]
+                    
+                    if pair in pair_to_words:
+                        pair_to_words[pair].discard(w)  # word not exists
+                        if not  pair_to_words[pair]:
+                            del pair_to_words[pair]
+
+            #updating word_freq
+            #adding the contribution of new words
+            new_word_freq = {}
+            for w in affected_words:
+                f = word_freq[w]
+                new_word = self.merge(w,best_pair,new_id)
+                new_word_freq[new_word] = new_word_freq.get(new_word,0) + f
+                for j in range(len(new_word)-1):
+                    pair = (new_word[j], new_word[j+1])
+                    pair_freq[pair] = pair_freq.get(pair, 0) + f
+
+                    if pair not in pair_to_words:
+                        pair_to_words[pair] = set()
+                    pair_to_words[pair].add(new_word)
+                    #heapq.heappush(heap, (-pair_freq[pair], pair))
+
+            #removing old words, adding new words
+            for word in affected_words:
+                del word_freq[word]
+
+            for new_word, freq in new_word_freq.items():
+                word_freq[new_word] = word_freq.get(new_word,0) + freq
+        
+
     def encode(self,text):
-        ids=[]
-        for token in re.findall(GPT2_PATTERN,text):
-            word_ids=list(token.encode("utf-8"))
-            while True:
-                pairs = [(word_ids[i], word_ids[i+1]) for i in range(len(word_ids)-1)]
-                best = min(
-                (p for p in pairs if p in self.merge_rules),
-                key=lambda p: self.merge_rules[p],
-                default=None
-                )   
-                if best is None: break
-                else: word_ids=merge( word_ids,best, self.merge_rules[best])
+        tokens = GPT2_REGEX.findall(text)
+        ids = []
+
+        for token in tokens:
+            word_ids = list(token.encode('utf-8'))
+
+            while (len(word_ids) >= 2):
+                best_pair = None
+                best_rank = float('inf')
+
+                for i in range(len(word_ids)-1):
+                    pair = (word_ids[i], word_ids[i+1])
+                    if pair in self.merge_rules:
+                        rank = self.merge_rules[pair]
+                        if rank < best_rank:
+                            best_rank = rank
+                            best_pair = pair
+
+                if best_pair is None:
+                    break
+                #best_pair = min(pairs_to_merge, key=lambda x: self.merge_rules[x]) #smallest idx (same order as the training)
+
+                new_word_ids = []
+                i =0 
+                while i<len(word_ids):
+                    if i < (len(word_ids)-1) and word_ids[i] == best_pair[0] and word_ids[i+1] == best_pair[1]:
+                        new_word_ids.append(self.merge_rules[best_pair])  # 
+                        i += 2
+                    else:
+                        new_word_ids.append(word_ids[i])
+                        i += 1
+                word_ids = new_word_ids
             ids.extend(word_ids)
-        return ids
+
+        return ids 
     
-    def decode(self,ids):
-        text=[]
-        for id in ids:
-            text.append(self.vocab[id])
-        return b"".join(text).decode("utf-8")
+
+    def decode(self, ids):
+        text_bytes = b''.join(self.vocab[idx] for idx in ids)
+        text = text_bytes.decode('utf-8', errors='replace')
+        return text
     
+
     def save(self, path):
-      import json
-      data = {
-          "merge_rules": {f"{p[0]},{p[1]}": idx for p, idx in self.merge_rules.items()}
-      }
-      with open(path, "w", encoding="utf-8") as f:
-          json.dump(data, f)
+        #json only take string as key, while self.merge_rules takes tuple 
+        # 1st step: convert tuple key into string
+        save_rules = {}
+        for pair, idx in self.merge_rules.items():
+            #print(pair)
+            key = f'{pair[0]},{pair[1]}'
+            save_rules[key] = idx
+        
+        #self.vocab takes bytes as value (b'th'), convert them into list
+        save_vocab = {}
+        for idx, text_bytes in self.vocab.items():
+            save_vocab[str(idx)] = list(text_bytes)
 
+        data_to_save = {
+            'merge_rules' : save_rules,
+            'vocab' : save_vocab
+        }
+        with open(path,'w', encoding='utf-8') as f:
+            json.dump(data_to_save, f , indent=2)
+        print(f'tokenizer saved to {path}')
+
+    
     def load(self, path):
-      import json
-      with open(path, "r", encoding="utf-8") as f:
-          data = json.load(f)
-      self.merge_rules = {
-          tuple(int(x) for x in k.split(",")): v
-          for k, v in data["merge_rules"].items()
-      }
-      self.vocab = {}
-      for i in range(256):
-          self.vocab[i] = bytes([i])
-      for pair, idx in sorted(self.merge_rules.items(), key=lambda x: x[1]):
-          self.vocab[idx] = self.vocab[pair[0]] + self.vocab[pair[1]]
+        with open(path, 'r', encoding='utf-8') as f:
+            loaded_data = json.load(f)
 
-# if __name__ == "__main__":
-#       tokenizer = BPETokenizer()
-#       tokenizer.train("the quick brown fox jumps over the lazy dog. " * 200, vocab_size=276)
-#       encoded = tokenizer.encode("the quick brown fox")
-#       decoded = tokenizer.decode(encoded)
-#       print(decoded)
-#       print(decoded == "the quick brown fox")
-#       print("Vocab size:", len(tokenizer.vocab))
+        # convert string back to tuples
+        self.merge_rules = {}
+        for key, val in loaded_data['merge_rules'].items():
+            pair = tuple(int(x) for x in key.split(','))
+            self.merge_rules[pair] = val
+        
+        # convert list back to bytes
+        self.vocab ={}
+        for key, val in loaded_data['vocab'].items():
+            key = int(key)
+            val = bytes(val)
+            self.vocab[key] = val
+        
+        print(f'tokenizer loaded from {path}')
+
+
+#-------------------------------------------------------------------------------------------------------------------
+#tokenizer = Tokenizer()
+# tokenizer.train(r'C:\Users\xhu70\Documents\LLM_from_scratch\data', vocab_size=50304)
+# tokenizer.save('my_tokenizer.json')
+# tokenizer.encode('hellow world')
+# print(tokenizer.decode(tokenizer.encode('hellow world')) == 'hellow world')
+          
